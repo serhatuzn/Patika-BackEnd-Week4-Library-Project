72fc684142801ca40faf20fd9525f0d220cb3d24
Serhatuzn <seerhatuzun@gmail.com>
2024-10-26 23:09:47 +0300

    Library Project

---
+++ Patika Kütüphane Project/Program.cs
@@ -0,0 +1,8 @@
+﻿using Patika_Kütüphane_Project;
+
+Kutuphane kitap1 = new Kutuphane("Aylin", "Ayşe", "Kulin", 398, "Remiz Kitapevi");
+
+kitap1.Yazdır();
+
+Kutuphane kitap2 = new Kutuphane("Bir Hırtonun Günü", "Serhat", "Uzun", 404, "Uzun Kitapevi");
+kitap2.Yazdır();
---
+++ Patika Kütüphane Project/kutuphane.cs
@@ -0,0 +1,61 @@
+namespace Patika_Kütüphane_Project
+{
+    public class Kutuphane
+    {
+        // Propertiler 
+        private int _SayfaSayısı;
+        public string KitapAdı { get; set; }
+        public string YazarAdı { get; set; }
+        public string YazarSoyadı { get; set; }
+        public string YayınEvi { get; set; }
+        public DateTime YayınTarihi { get; set; }
+
+        public int SayfaSayısı
+        {
+            get => _SayfaSayısı;
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Geçersiz Sayfa Sayısı");
+                }
+                else
+                {
+                    _SayfaSayısı = value;
+                }
+            }
+        }
+
+        // Default Constructor
+        public Kutuphane()
+        {
+            YayınTarihi = DateTime.Now;
+        }
+
+        // Parametreli Constructor
+        public Kutuphane(string kitapAdı, string yazarAdı, string yazarSoyadı, int sayfaSayısı, string yayınEvi)
+        {
+            // Constructor parametrelerini property'lere atıyoruz
+            this.KitapAdı = kitapAdı;
+            this.YazarAdı = yazarAdı;
+            this.YazarSoyadı = yazarSoyadı;
+            this.SayfaSayısı = sayfaSayısı;
+            this.YayınEvi = yayınEvi;
+            this.YayınTarihi = DateTime.Now.Date;
+        }
+
+        public void Yazdır()
+        {
+            string tarih = YayınTarihi.ToString("dd.MM.yyyy");
+            Console.WriteLine($"Kitap Adı: {KitapAdı} | Yazar Adı: {YazarAdı} | Yazar Soyadı: {YazarSoyadı} | " +
+                              $"Sayfa Sayısı: {SayfaSayısı} | Yayın Evi: {YayınEvi} | Kayıt Tarihi: {tarih}");
+        }
+    }
+    //Class: Benzer özelliklere ve davranışlara sahip nesneleri tanımlayan bir şablondur.
+
+    //Property: Sınıf içinde tanımlanan, nesne verilerine erişim sağlayan değişkenlerdir.
+
+    //New: Yeni bir nesne oluşturmak için kullanılan anahtar kelimedir.
+
+    //Constructor: Bir sınıfın nesnesi oluşturulduğunda otomatik olarak çağrılan ve başlangıç değerlerini atayan özel bir metottur.
+}
